95190501da23ec769594a7abf356d2d9a6c1ef74
Criss <cristinaguzman@live.cl>
2022-07-15 23:10:19 -0400

    avance vista1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import HomeViewUno from '../views/HomeViewUno.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,8 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    component: HomeViewUno,
   },
   {
     path: '/about',
@@ -17,7 +19,18 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    
+  },
+  {
+    path: '/HomeViewUno',
+    name: 'HomeViewUno',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "HomeViewUno" */ '../views/HomeViewUno.vue')
+    
   }
+  
 ]
 
 const router = new VueRouter({
