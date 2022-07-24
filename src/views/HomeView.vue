83a41e1822a1b10750943d396af3248fd48fb285
Ale <alegonzalez1993@gmail.com>
2022-07-24 18:24:47 -0400

    Add map component with control

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,33 @@
 <template>
-  <v-layout style="gap: 10px" wrap>
-    <v-flex v-for="parking in parkings" :key="parking.id">
-      <CardParking :value="parking"/>
-    </v-flex>
-  </v-layout>  
+  <v-container>
+    <v-row>
+      <v-col>
+      <v-layout id="mapContainer">
+        <MapComponent/>
+      </v-layout>
+        
+      </v-col>
+      <v-col>
+        <v-layout style="gap: 10px" wrap>
+          <v-flex v-for="parking in parkings" :key="parking.id">
+            <CardParking :value="parking"/>
+          </v-flex>
+        </v-layout>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 
 import CardParking from "@/components/CardParking.vue"
+import MapComponent from "@/components/MapComponent.vue"
 
 import { mapState, mapActions } from 'vuex'
 export default {
-    components: { 
-      CardParking 
-    },
+    components: {
+    CardParking,
+    MapComponent
+},
     computed: {
       ...mapState('parkings', {
         parkings: (state) => state.list
@@ -31,4 +45,8 @@ export default {
 </script>
 
 <style>
+#mapContainer{
+  width: 100%;
+  height: 100%;
+}
 </style>
