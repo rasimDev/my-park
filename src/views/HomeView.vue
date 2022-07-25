7fbbb29b53610c2c6ce769ef2f25989c7f6f3cdb
Rai Silva <rainiersilva14@gmail.com>
2022-07-24 23:19:19 -0400

    conflictos resueltos 100

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,29 @@
 <template>
-  <v-layout style="gap: 10px" wrap>
-    <v-flex v-for="parking in parkings" :key="parking.id">
-      <CardParking :value="parking"/>
-    </v-flex>
-  </v-layout>  
-  <v-container>
-    <v-row>
-      <v-col>
-      <v-layout id="mapContainer">
-        <MapComponent/>
-      </v-layout>
-        
-      </v-col>
-      <v-col>
-        <v-layout style="gap: 10px" wrap>
-        <AddParkingBtn/>
-          <v-flex v-for="parking in parkings" :key="parking.id">
-            <CardParking :value="parking"/>
-          </v-flex>
-        </v-layout>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-layout id="mapContainer">
+            <MapComponent />
+          </v-layout>
+        </v-col>
+        <v-col>
+          <v-layout style="gap: 10px" wrap>
+            <AddParkingBtn />
+            <v-flex v-for="parking in parkings" :key="parking.id">
+              <CardParking :value="parking" />
+            </v-flex>
+          </v-layout>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 <script>
 
 import CardParking from "@/components/CardParking.vue"
-
-import { mapState, mapActions } from 'vuex'
-export default {
-    components: { 
-      CardParking 
-    },
 import MapComponent from "@/components/MapComponent.vue"
 import AddParkingBtn from "@/components/AddParkingBtn.vue"
-
 import { mapState, mapActions } from 'vuex'
 export default {
     components: {
@@ -59,7 +48,7 @@ export default {
 </script>
 
 <style>
-#mapContainer{
+#mapContainer {
   width: 100%;
   height: 100%;
 }
